d1f748352c4166a1875bf09fb199d18f174fff12
NEERAJ SINGH RAJPUT <neeraj.rajput@tinyscript.in>
2024-06-30 20:59:08 +0530

    final preview

---
+++ src/app/_components/Hero.jsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+export default function Hero() {
+  return (
+    <section
+      id="hero"
+      className="flex h-[80vh] flex-col md:flex-row items-center justify-between px-[5%] space-y-24 py-[10vh]"
+    >
+      {/* Left Section */}
+      <div className="flex flex-col space-y-4 md:w-3/5">
+        <h1 className="heading">Chaitali Mewada </h1>
+        <p className="paragraph">
+          I'm a passionate web developer and designer with experience in
+          building web applications. Check out my work and get in touch!
+        </p>
+        <Link
+          href="mailto:your-email@example.com"
+          className="button-opposite w-fit"
+        >
+          CONTACT ME
+        </Link>
+      </div>
+
+      {/* Right Section */}
+      <div className="md:w-2/5 flex justify-end items-center">
+        <div className="w-fit p-4 "></div>
+      </div>
+    </section>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,19 @@
+import AboutMe from "./_components/About";
+import Footer from "./_components/Footer";
+import Header from "./_components/Header";
+import Hero from "./_components/Hero";
+import Portfolio from "./_components/Portfolio";
+import Skills from "./_components/Skills";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <AboutMe />
+      <Skills />
+      <Portfolio />
+      <Footer />
+    </>
+  );
+}
